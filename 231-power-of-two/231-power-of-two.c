c3a94c9e236c2f34c969f9b18eb5ba6dd9de45a1
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-08-05 00:12:07 +0530

    Time: 0 ms (100.00%), Space: 5.4 MB (97.74%) - LeetHub

--- 231-power-of-two/231-power-of-two.c
+++ 231-power-of-two/231-power-of-two.c
@@ -1,18 +1,8 @@
 
-bool check(int n)
-{
-    if(n==2)
-        return true;
-    else if(n%2==0)
-    return check(n/2);
-    return false;
-}
-bool isPowerOfTwo(int n){
+bool isPowerOfTwo(long int n){
     if(n==0)
         return false;
-    if(n==1||n==2)
-        return true;
-    if(check(n))
+    if((n&(n-1))==0)
         return true;
     return false;
 }
