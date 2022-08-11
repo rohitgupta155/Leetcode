842a7877575b493b19c78788aefe8319cafa6857
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-08-11 12:07:02 +0530

    Time: 23 ms (92.81%), Space: 19.3 MB (57.19%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size(),l=nums.size()-1;
+        int n=nums.size(),z=0,j=0;
         
         for(int i=0;i<n;i++)
         {
             if(nums[i]==0)
             {
-                nums.erase(nums.begin()+i);
-                nums.push_back(0);
-                i--;
-                l--;
+                z++;
             }
-            if(i==l)
-                break;
+            else{
+                nums[j++]=nums[i];
+            }
         }
+        for(int i=0;i<z;i++)
+            nums[j++]=0;
     }
 };
