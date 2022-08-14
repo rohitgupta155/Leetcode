aace0b96bd7144950ed09de13502979d62b36150
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-08-14 22:44:47 +0530

    Time: 41 ms (24.32%), Space: 18 MB (19.70%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& a) {
         int n=a.size();
-        int total=n*(n+1)/2,sum=0;
+        int total=n*(n+1)>>1,sum=0;
         for(int i=0;i<n;i++)
         sum+=a[i];
         return total-sum;
