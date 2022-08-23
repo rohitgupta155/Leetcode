236ca391e88ff01e3a190b2117c64b43e88b89cf
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-08-24 02:01:11 +0530

    Time: 19 ms (41.40%), Space: 16.5 MB (13.16%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -4,23 +4,35 @@ public:
         if(a.size()==1)
             return a;
         queue<int>o,e;
-        int n=a.size(),j=0;
-            for(int i=0;i<n;i++)
+        int n=a.size()-1,j=0;
+        while(j<n)
         {
-            if(a[i]%2==0)
-                e.push(a[i]);
-                else o.push(a[i]);
+            if(a[j]%2==1&&a[n]%2==0)
+                swap(a[j++],a[n--]);
+            if(a[j]%2==0)
+            {
+                j++;
+            }
+            if(a[n]%2==1)
+                n--;
+            
         }
-        while(!e.empty())
-        {
-            a[j++]=e.front();
-            e.pop();
-        }
-        while(!o.empty())
-        {
-            a[j++]=o.front();
-            o.pop();
-        }
+        //     for(int i=0;i<n;i++)
+        // {
+        //     if(a[i]%2==0)
+        //         e.push(a[i]);
+        //         else o.push(a[i]);
+        // }
+        // while(!e.empty())
+        // {
+        //     a[j++]=e.front();
+        //     e.pop();
+        // }
+        // while(!o.empty())
+        // {
+        //     a[j++]=o.front();
+        //     o.pop();
+        // }
         
         return a;
     }
