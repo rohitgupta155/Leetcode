0d522f3b43268d82eb11f306b5cb4fb4b4352719
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-08-24 02:04:23 +0530

    Time: 18 ms (46.08%), Space: 16.4 MB (22.19%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -3,20 +3,28 @@ public:
     vector<int> sortArrayByParity(vector<int>& a) {
         if(a.size()==1)
             return a;
-        queue<int>o,e;
-        int n=a.size()-1,j=0;
-        while(j<n)
-        {
-            if(a[j]%2==1&&a[n]%2==0)
-                swap(a[j++],a[n--]);
-            if(a[j]%2==0)
-            {
-                j++;
-            }
-            if(a[n]%2==1)
-                n--;
+        int n=a.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+            if(a[i]%2==0)
+                ans.push_back(a[i]);
+        for(int i=0;i<n;i++)
+            if(a[i]%2==1)
+                ans.push_back(a[i]);
+//         queue<int>o,e;
+//         int n=a.size()-1,j=0;
+//         while(j<n)
+// //         {
+// //             if(a[j]%2==1&&a[n]%2==0)
+// //                 swap(a[j++],a[n--]);
+// //             if(a[j]%2==0)
+// //             {
+// //                 j++;
+// //             }
+// //             if(a[n]%2==1)
+// //                 n--;
             
-        }
+// //         }
         //     for(int i=0;i<n;i++)
         // {
         //     if(a[i]%2==0)
@@ -34,6 +42,6 @@ public:
         //     o.pop();
         // }
         
-        return a;
+        return ans;
     }
 };
