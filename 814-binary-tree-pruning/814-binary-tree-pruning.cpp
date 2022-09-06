c0d7f15bf47f8a8decbabda1b155a1b39ed03c9c
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-09-06 13:59:45 +0530

    Time: 3 ms (71.58%), Space: 8.8 MB (64.29%) - LeetHub

---
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(!root->left&&!root->right&&root->val==0)
+            return NULL;
+        prune(root,root);
+        if(!root->left&&!root->right&&root->val==0)
+            return NULL;
+        return root;
+    }
+    void prune(TreeNode* (&r),TreeNode* (&p))
+    {
+        if(!r)
+            return;
+        prune(r->left,r);
+        prune(r->right,r);  
+        if(p->left&&!(p->left->left)&&!(p->left->right)&&p->left->val==0)
+            {cout<<p->val<<" "<<p->left->val<<endl;
+            p->left=NULL;
+        }
+ if(p->right&&!(p->right->left)&&!(p->right->right)&&p->right->val==0)
+            {cout<<p->val<<" "<<p->right->val<<endl;p->right=NULL;
+            }
+    }
+};
