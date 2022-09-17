e7ea10197cb9acd6c9009432d5f89103dca00afb
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-09-17 23:52:52 +0530

    Time: 177 ms (59.52%), Space: 38.3 MB (73.94%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
         unordered_map<int,int> a;
+			/*Used unordered map to count the even element's occurrence*/
         for(auto i:nums)
           { if(i%2==0)
             a[i]++;}
+			 if(a.size()==0)   
+            return -1;
         int m=INT_MIN;
+	   /* Found the max frequency from map*/
         for(auto i:a)
             m=max(m,i.second);
-        vector<int>v;
+        vector<int>v;   //Assigned new vector to store elements with max frequency
         for(auto i:a){
             if(i.second==m)
                 v.push_back(i.first);
            
         } int mi=INT_MAX;
-            for(auto i:v)
+            for(auto i:v)        //Finding minimum element from elements with maximum frequency
                 mi=min(i,mi);
-        if(mi==INT_MAX)
-            return -1;
-            return mi;
+       
+            return mi;   //return minimum even element
     }
 };
