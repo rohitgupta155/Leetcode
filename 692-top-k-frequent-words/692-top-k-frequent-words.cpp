e2a2aa6e060b2e6056cad2f2c9deb44722296e16
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-09-18 00:56:08 +0530

    Time: 48 ms (5.31%), Space: 12.8 MB (17.99%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& n, int k) {
+         unordered_map<string,int> m;
+        vector<pair<int,string>> mi;
+        vector<string> ans;
+        int l=n.size();
+        for(int i=0;i<l;i++)
+            m[n[i]]++;
+        for(auto i:m)
+            cout<<i.first<<"->"<<i.second<<endl;
+        for(auto i:m)
+            {
+            mi.push_back(make_pair(-i.second,i.first));
+        }
+        sort(mi.begin(),mi.end());
+        cout<<endl;
+        for(auto i:mi)
+            cout<<i.first<<"->"<<i.second<<endl;
+        int j=0;
+        for(auto i:mi)
+            {
+            j++;
+            ans.push_back(i.second);
+             if(j==k)
+                 break;
+        }
+        return ans;
+        
+    }
+};
