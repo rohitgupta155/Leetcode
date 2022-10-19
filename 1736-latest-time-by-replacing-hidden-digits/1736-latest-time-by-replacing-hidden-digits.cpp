808fa4daadbf0f271393cbd7967b1e605cfe2287
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-10-19 17:00:22 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (70.33%) - LeetHub

---
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string maximumTime(string time) {
+        if(time[0]=='?'){
+            if(time[1]=='?')
+                time[0]='2',time[1]='3';
+            else if(time[1]<'4')
+                time[0]='2';
+            else time[0]='1';}
+        if(time[1]=='?')
+        {
+            if(time[0]<'2')
+                time[1]='9';
+            else time[1]='3';
+        }
+        if(time[3]=='?')
+        {
+            if(time[4]=='?')
+                time[3]='5',time[4]='9';
+            else time[3]='5';
+        }
+        if(time[4]=='?')
+            time[4]='9';
+        return time;
+    }
+};
