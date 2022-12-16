848195516cc53a7c2ee7b94fed2aa292085e1b36
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2022-12-16 17:53:40 +0530

    Time: 1292 ms (52.10%), Space: 294.9 MB (5.03%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+   bool f=false;
+    unordered_map<int,vector<int>> m;
+    unordered_set<int> v;
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        if(source==destination)
+            return true;
+        v.insert(source);
+        for(auto i:edges)
+        {
+            m[i[0]].push_back(i[1]);            
+            m[i[1]].push_back(i[0]);            
+        }
+        cout<<source<<"->";
+        for(auto i:m[source])
+       { if(!f&&v.find(i)==v.end()){cout<<i<<"->";v.insert(i);find(i,destination);}
+         else break;}
+        return f;
+    }
+    void find(int s,int d)
+    {
+        if(s==d){
+            f=true;
+            return;}
+        for(auto i:m[s])
+         {  if(v.find(i)==v.end()){cout<<i<<"->";v.insert(i); find(i,d);}
+          }
+    }
+};
