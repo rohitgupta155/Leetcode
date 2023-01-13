2d63861a157dd34926e96d0ecfa0114c62818c1e
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2023-01-13 11:25:48 +0530

    Time: 12 ms (33.90%), Space: 5.8 MB (84.96%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int arrangeCoins(long int n) {
+        long int c=1,i=0,row=0;
+        while(c)
+        {
+            if(i>n)
+            return row-1; 
+            else if(i==n)
+                return row;
+            i=i+c;
+            c++;
+            row++;
+
+        }  
+        return 0;
+    }
+};
