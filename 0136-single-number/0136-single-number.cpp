61bebf0079a3c42e2b014163dde5a884e7520ce6
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2023-02-17 08:31:05 +0530

    Time: 39 ms (23.40%), Space: 20.7 MB (6.45%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+            m[nums[i]]++;
+        for(auto i:m)
+            if(i.second==1)
+                return i.first;
+        return 0;
+    }
+};
