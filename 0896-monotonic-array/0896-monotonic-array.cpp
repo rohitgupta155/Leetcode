090861433fbe59a106704fc9ae51fc6a35e9f731
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2023-09-29 17:09:17 +0530

    Time: 143 ms (7.80%), Space: 96.6 MB (29.47%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -3,22 +3,36 @@ public:
     bool isMonotonic(vector<int>& nums) {
         if(nums.size()<3)
             return true;
-        int n=nums.size(),c=0;
-        for(int i=0;i<n-1;i++)
+        // int n=nums.size(),c=0;
+        // for(int i=0;i<n-1;i++)
+        // {
+        //     if(nums[i]<=nums[i+1])
+        //         c++;
+        //         if(c==(n-1))
+        //             return true;
+        // }
+        // c=0;
+        // for(int i=0;i<n-1;i++)
+        // {
+        //     if(nums[i]>=nums[i+1])
+        //         c++;
+        //         if(c==(n-1))
+        //             return true;
+        // }
+        // return false;
+        int i=0;
+        while(i<nums.size()-1&&nums[i]==nums[i+1])
+            i++;
+        if(i>=nums.size()-1)return true;
+        bool parity=(nums[i]<nums[i+1]);
+        cout<<parity;
+        for(;i<nums.size()-1;i++)
         {
-            if(nums[i]<=nums[i+1])
-                c++;
-                if(c==(n-1))
-                    return true;
+            if(nums[i]==nums[i+1])continue;
+            bool check=(nums[i]<nums[i+1]);
+            if(check^parity)
+                return false;
         }
-        c=0;
-        for(int i=0;i<n-1;i++)
-        {
-            if(nums[i]>=nums[i+1])
-                c++;
-                if(c==(n-1))
-                    return true;
-        }
-        return false;
+        return true;
     }
 };
