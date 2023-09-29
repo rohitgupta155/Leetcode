50184f1c9d281515ee462d0db540e5783f3894b1
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2023-09-29 17:09:46 +0530

    Time: 123 ms (56.83%), Space: 96.8 MB (22.05%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -3,29 +3,11 @@ public:
     bool isMonotonic(vector<int>& nums) {
         if(nums.size()<3)
             return true;
-        // int n=nums.size(),c=0;
-        // for(int i=0;i<n-1;i++)
-        // {
-        //     if(nums[i]<=nums[i+1])
-        //         c++;
-        //         if(c==(n-1))
-        //             return true;
-        // }
-        // c=0;
-        // for(int i=0;i<n-1;i++)
-        // {
-        //     if(nums[i]>=nums[i+1])
-        //         c++;
-        //         if(c==(n-1))
-        //             return true;
-        // }
-        // return false;
         int i=0;
         while(i<nums.size()-1&&nums[i]==nums[i+1])
             i++;
         if(i>=nums.size()-1)return true;
         bool parity=(nums[i]<nums[i+1]);
-        cout<<parity;
         for(;i<nums.size()-1;i++)
         {
             if(nums[i]==nums[i+1])continue;
