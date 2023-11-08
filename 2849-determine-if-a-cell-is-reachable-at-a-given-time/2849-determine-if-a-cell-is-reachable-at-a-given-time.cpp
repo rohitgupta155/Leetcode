83faef7c24df7f8fc11ed9edc23b0aae7a74511d
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2023-11-08 08:23:35 +0530

    Time: 4 ms (29.27%), Space: 6.2 MB (15.73%) - LeetHub

---
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        if(sx==fx&&sy==fy)
+            return t!=1;
+        return t>=max(abs(sx-fx),abs(sy-fy));
+    }
+};
