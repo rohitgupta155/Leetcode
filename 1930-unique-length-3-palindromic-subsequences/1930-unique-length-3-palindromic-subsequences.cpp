51ab1356ff2ea7de91dde0466b1e792107df8e1d
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2023-11-14 09:50:18 +0530

    Time: 1448 ms (7.77%), Space: 14.9 MB (26.48%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        unordered_map<char,int> r,l;
+        unordered_set<string> c;
+        for(int i=s.size()-1;i>1;i--)
+            r[s[i]]++;
+        l[s[0]]++;
+        for(int i=1;i<s.size()-1;i++)
+        {
+            for(char j='a';j<='z';j++)
+            {
+                if(l.count(j)!=0&&r.count(j)!=0)
+                    c.insert({j,s[i],j});
+            }
+            l[s[i]]++;
+            r[s[i+1]]--;
+            if(r[s[i+1]]==0)
+                r.erase(s[i+1]);
+        }
+        for(auto i:c)
+            cout<<i<<endl;
+        return c.size();
+        
+    }
+};
