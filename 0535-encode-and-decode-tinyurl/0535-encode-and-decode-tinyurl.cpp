d9769111027e90b937772e13dc5edf9817df5162
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2023-12-17 13:55:38 +0530

    Time: 3 ms (69.43%), Space: 7.7 MB (19.07%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,36 @@
 class Solution {
 public:
-
+    
     // Encodes a URL to a shortened URL.
+    unordered_map<int,string> m;
+    int i=1;
+    string c="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     string encode(string longUrl) {
+        m[i]=longUrl;
+        longUrl.clear();
+        int n=i;
+        i++;
+        while(n)
+        {
+            longUrl=c[n%62]+longUrl;
+            n/=62;
+        }
         return longUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        int n=0;
+        for(auto i:shortUrl)
+        {
+            if(i>='a'&&i<'z')
+                n=n*62+i-'a';
+            else if(i>='A'&&i<='Z')
+                n=n*62+i-'A';
+            else
+                n=n*62+i-'0';
+        }
+        return m[n];
     }
 };
 
