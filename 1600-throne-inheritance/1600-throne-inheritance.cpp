7de05a3137d4a0bf366005362ef8fa3e23032e77
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2024-01-12 03:20:29 +0530

    Time: 440 ms (51.97%), Space: 169.4 MB (60.53%) - LeetHub

---
+++ 1600-throne-inheritance/1600-throne-inheritance.cpp
@@ -0,0 +1,48 @@
+// class kigdomCum{
+//     public:
+//     string baap;
+//     vector<kingdomCum* >beta;
+// }
+class ThroneInheritance {
+public:
+    // kingdomCum raja;
+    unordered_map<string,vector<string>> rajPariwar;
+    unordered_set<string> samsan;
+    string raja;
+    ThroneInheritance(string kingName) {
+        // raja=new kingdomCum();
+        // raja.baap=kingName;
+        raja=kingName;
+    }
+    
+    void birth(string baap, string beta) {
+        rajPariwar[baap].push_back(beta);
+    }
+    
+    void death(string name) {
+        samsan.insert(name);
+    }
+    
+    vector<string> getInheritanceOrder() {
+        vector<string> waaris;
+        warisnikalo(raja,waaris);
+        return waaris;
+    }
+    void warisnikalo(string raaja,vector<string> &waaris)
+    {
+        if(samsan.find(raaja)==samsan.end())
+            waaris.push_back(raaja);
+        for(auto i:rajPariwar[raaja])
+        {
+            warisnikalo(i,waaris);
+        }
+    }
+};
+
+/**
+ * Your ThroneInheritance object will be instantiated and called as such:
+ * ThroneInheritance* obj = new ThroneInheritance(kingName);
+ * obj->birth(parentName,childName);
+ * obj->death(name);
+ * vector<string> param_3 = obj->getInheritanceOrder();
+ */
