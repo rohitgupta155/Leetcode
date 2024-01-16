7c8ec0348dac8fefb37bdf8c80cddee715912e75
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2024-01-16 17:28:01 +0530

    Time: 86 ms (38.27%), Space: 17.6 MB (18.93%) - LeetHub

--- 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.cpp
+++ 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     bool isItPossible(string word1, string word2) {
-        if(word1.size()==1&&word2.size()==1)
-            return true;
+        
         unordered_map<char,int> w1,w2;
         for(auto i:word1)
            w1[i]++;
         for(auto i:word2)
            w2[i]++;
-        // if(w1.size()==w2.size())
-        // {
-        //     if(w1.size()>1)
-        //         return true;
-        //     if(w1.begin()->first==w2.begin()->first)
-        //         return true;
-        //     return w1.begin()->second>1&&w2.begin()->second>1;
-        // }
+        
         for(char i='a';i<='z';i++)
         {
             if(!w1.count(i))
@@ -25,7 +17,6 @@ public:
                 w1.erase(i);
             for(char j='a';j<='z';j++)
             {
-                // if(i!=j){
                     if(w2.count(j)){
                         w1[j]++;
                         w2[j]--;
@@ -43,8 +34,6 @@ public:
                             w1.erase(j);
                         
                     }
-                    
-                // }
             }
             w1[i]++;
         }
