10d6e5781daca3c7aa24d73de800f0ae97a2dcf2
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2024-02-12 20:36:15 +0530

    Time: 7 ms (97.54%), Space: 22.1 MB (24.81%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> m;
+        for(auto i:nums)
+            m[i]++;
+        for(auto i:m){
+            if(i.second>nums.size()/2)
+                return i.first;
+        }
+        return -1;
+    }
+};
