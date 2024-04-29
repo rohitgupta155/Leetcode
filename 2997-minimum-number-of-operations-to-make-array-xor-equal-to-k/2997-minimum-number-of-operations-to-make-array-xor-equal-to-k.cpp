d5c725ad8cc8fa01f507384307669a52c49270cc
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2024-04-30 02:49:34 +0530

    Time: 144 ms (23.59%), Space: 91.7 MB (8.27%) - LeetHub

---
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void takeout(int n,vector<int> &v)
+    {
+        int i=31;
+        while(n)
+        {
+            v[i]+=(n%2);
+            n/=2;
+            i--;
+        }
+    }
+    int minOperations(vector<int>& nums, int k) {
+        vector<int> v(32,0),r(32,0);
+        for(auto i:nums)
+        {
+            takeout(i,v);
+        }
+        takeout(k,r);
+        int ans=0;
+        for(int i=0;i<32;i++)
+        {
+            if(v[i]%2!=r[i]%2)
+                ans++;
+        }
+        return ans;
+    }
+};
