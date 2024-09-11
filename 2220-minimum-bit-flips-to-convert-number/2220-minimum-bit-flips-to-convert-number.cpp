469ae4389ffdf6ef81d13b1f97ff4f6462a237b0
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2024-09-11 18:55:46 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (44.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,28 +1,7 @@
 class Solution {
 public:
-    int minBitFlips(int a, int b) {
-        string A=DecimalToBinaryString(a);   
-        string B=DecimalToBinaryString(b);
-        int count=0;
-        for(int i=0;i<32;i++)
-        {
-            if(A[i]!=B[i])
-                count++;
-        }
-       return count;
+    int minBitFlips(int start, int goal) {
+        int a=start^goal;
+        return __builtin_popcount(a);
     }
-   string DecimalToBinaryString(int a)
-{
-    string binary = "";
-    int mask = 1;
-    for(int i = 0; i < 31; i++)
-    {
-        if((mask&a) >= 1)
-            binary = "1"+binary;
-        else
-            binary = "0"+binary;
-        mask<<=1;
-    }
-    return binary;
-}
 };
