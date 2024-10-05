c676f9aa1fab9582f81a7c52eaca09b18203a074
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2024-10-05 23:27:44 +0530

    Time: 363 ms (11.68%), Space: 144.7 MB (5.01%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,15 +3,20 @@ public:
     bool checkInclusion(string s1, string s2) {
         if(s1.size()>s2.size())
             return false;
-        unordered_map<char,int> m;
+        unordered_map<char,int> m,t;
         for(auto i:s1)
             m[i]++;
-        for(int i=0;i<=s2.size()-s1.size();i++)
-        {
-            unordered_map<char,int> t;
-            for(int j=i;j<s1.size()+i;j++){
+        for(int j=0;j<s1.size();j++){
                 t[s2[j]]++;
             }
+        if(matches(m,t))
+            return true;
+        for(int i=s1.size();i<=s2.size();i++)
+        {
+            t[s2[i-s1.size()]]--;
+            if(t[s2[i-s1.size()]]==0)
+                t.erase(s2[i-s1.size()]);
+            t[s2[i]]++;
             if(matches(m,t))
                 return true;
         }
