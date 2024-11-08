dac7e87ce4d0dc47aaeb02f349c914ad891bd00e
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2024-11-08 13:09:34 +0530

    Time: 32 ms (10.48%), Space: 99.9 MB (16.72%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int m) {
+        int a=0,n=pow(2,m)-1;
+        for(auto i:nums)
+            a^=i;
+        vector<int> ans;
+        for(int i=1;i<=nums.size();i++){
+            int t=1,curr=0,temp=a;
+            while(t<=n)
+            {
+                if(temp%2==0)
+                    curr^=t;
+                t<<=1;
+                temp>>=1;
+            }
+            ans.push_back(curr);
+            a^=nums[nums.size()-i];
+        }
+        return ans;
+    }
+};
