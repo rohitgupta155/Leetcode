31525e5cbbb64fefb557636a1ad8d09d7b2c49ee
Rohit Prasad Gupta <90511708+rohitgupta155@users.noreply.github.com>
2024-11-08 13:14:22 +0530

    Time: 0 ms (100.00%), Space: 99.8 MB (16.72%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -6,15 +6,16 @@ public:
             a^=i;
         vector<int> ans;
         for(int i=1;i<=nums.size();i++){
-            int t=1,curr=0,temp=a;
-            while(t<=n)
-            {
-                if(temp%2==0)
-                    curr^=t;
-                t<<=1;
-                temp>>=1;
-            }
-            ans.push_back(curr);
+            // int t=1,curr=0,temp=a;
+            // while(t<=n)
+            // {
+            //     if(temp%2==0)
+            //         curr^=t;
+            //     t<<=1;
+            //     temp>>=1;
+            // }
+            // ans.push_back(curr);  //what i thought of...its working stil...
+            ans.push_back(n^a); //faster one
             a^=nums[nums.size()-i];
         }
         return ans;
